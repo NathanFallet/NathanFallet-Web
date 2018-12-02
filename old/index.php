5928452ce4e4e6c6313179cafa5e6a6c8424c997
Nathan Fallet <contact@nathanfallet.me>
2018-12-02 11:02:57 +0100

    Bug correction

--- old/index.php
+++ old/index.php
@@ -1,12 +1,6 @@
 <?php
 
 $to = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-if(isset($_SERVER['HTTP_REFERER'])){
-  $from = $_SERVER['HTTP_REFERER'];
-  error_log("Access to $to from $from\n", 3, "/var/www/nathanfallet.me/old/logs.log");
-}
-
 $url = "https://www.nathanfallet.me/";
 
 $params = parse_url($to);
